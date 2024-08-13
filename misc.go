df97f916f87a99ae0cc9b002b4ba40a576db4ae8
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-13 10:44:59 +0800

    Update misc.go

---
+++ misc.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "io"
+    "net"
+)
+
+func handleConnections(conn1, conn2 net.Conn) {
+    done := make(chan struct{}, 2)
+    go func() {
+        defer conn1.Close()
+        io.Copy(conn1, conn2)
+        done <- struct{}{}
+    }()
+    go func() {
+        defer conn2.Close()
+        io.Copy(conn2, conn1)
+        done <- struct{}{}
+    }()
+    <-done
+    <-done
+}
