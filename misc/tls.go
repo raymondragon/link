4c87c0257fef069644f533a88afa9f56d6ba92d4
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-20 16:05:51 +0800

    Update and rename tls.go to tls.go

--- misc/tls.go
+++ misc/tls.go
@@ -1,4 +1,4 @@
-package tls
+package main
 
 import (
     "crypto/rand"
@@ -9,11 +9,9 @@ import (
     "encoding/pem"
     "math/big"
     "time"
-
-    "github.com/caddyserver/certmagic"
 )
 
-func TLSConfigGeneration(hostname string) (*tls.Config, error) {
+func tlsConfigGeneration(hostname string) (*tls.Config, error) {
     private, err := rsa.GenerateKey(rand.Reader, 2048)
     if err != nil {
         return nil, err
@@ -44,24 +42,4 @@ func TLSConfigGeneration(hostname string) (*tls.Config, error) {
     }
     tlsConfig := &tls.Config{Certificates: []tls.Certificate{cert}}
     return tlsConfig, nil
-}
-
-func TLSConfigApplication(username, hostname string) (*tls.Config, error) {
-    certmagic.DefaultACME.CA = certmagic.LetsEncryptProductionCA
-    certmagic.DefaultACME.Agreed = true
-    mailAddr := username + "@" + hostname
-    if username == "" {
-        mailAddr = "acme@" + hostname
-    }
-    certmagic.DefaultACME.Email = mailAddr
-    tlsConfig, err := certmagic.TLS([]string{hostname})
-    return tlsConfig, err
-}
-
-func TLSConfigSetup(username, hostname string) (*tls.Config, error) {
-    tlsConfig, err := TLSConfigApplication(username, hostname)
-    if err != nil {
-        tlsConfig, err = TLSConfigGeneration(hostname)
-    }
-    return tlsConfig, err
 }
