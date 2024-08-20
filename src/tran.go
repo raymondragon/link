c6a6ab562d78aa22b32526c6680d36eab6f62b2c
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-20 10:58:44 +0800

    Update and rename conn.go to tran.go

--- src/tran.go
+++ src/tran.go
@@ -5,7 +5,7 @@ import (
     "net"
 )
 
-func handleConnections(conn1, conn2 net.Conn) {
+func handleTransmissions(conn1, conn2 net.Conn) {
     done := make(chan struct{}, 2)
     go func() {
         defer conn1.Close()
