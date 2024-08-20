4ffb8b367e634a74882c4058d2e13f2b96a8a522
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-20 09:55:38 +0800

    Create client.go

---
+++ client.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "net"
+    "net/url"
+    "strings"
+)
+
+func runClient(parsedURL *url.URL) error {
+    linkAddr, err := net.ResolveTCPAddr("tcp", parsedURL.Host)
+    if err != nil {
+        return err
+    }
+    targetAddr, err := net.ResolveTCPAddr("tcp", strings.TrimPrefix(parsedURL.Path, "/"))
+    if err != nil {
+        return err
+    }
+    linkConn, err := net.DialTCP("tcp", nil, linkAddr)
+    if err != nil {
+        return err
+    }
+    linkConn.SetNoDelay(true)
+    targetConn, err := net.DialTCP("tcp", nil, targetAddr)
+    if err != nil {
+        linkConn.Close()
+        return err
+    }
+    targetConn.SetNoDelay(true)
+    handleConnections(linkConn, targetConn)
+    return nil
+}
