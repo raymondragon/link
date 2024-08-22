a1ebae6a0d92cf9cbe5c95ab714cff9dbe05c5f9
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-22 21:41:43 +0800

    Update transmissions.go

--- pkg/handle/transmissions.go
+++ pkg/handle/transmissions.go
@@ -5,7 +5,7 @@ import (
     "net"
 )
 
-func transmissions(conn1, conn2 net.Conn) {
+func Transmissions(conn1, conn2 net.Conn) {
     done := make(chan struct{}, 2)
     go func() {
         defer conn1.Close()
