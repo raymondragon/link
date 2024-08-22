d3179da98fa63cd274f2cd03c91733c661b520ed
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-22 16:46:31 +0800

    Create setup.go

---
+++ pkg/tlsconfig/setup.go
@@ -0,0 +1,13 @@
+package tlsconfig
+
+import (
+    "crypto/tls"
+)
+
+func Setup(username, hostname string) (*tls.Config, error) {
+    tlsConfig, err := Application(username, hostname)
+    if err != nil {
+        tlsConfig, err = Generation(hostname)
+    }
+    return tlsConfig, err
+}
