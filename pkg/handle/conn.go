968fb4b165b296bff0d29a2bcd8a15284b901e19
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-24 23:39:04 +0800

    Update and rename transmissions.go to conn.go

--- pkg/handle/conn.go
+++ pkg/handle/conn.go
@@ -5,7 +5,7 @@ import (
     "net"
 )
 
-func Transmissions(conn1, conn2 net.Conn) {
+func Conn(conn1, conn2 net.Conn) {
     done := make(chan struct{}, 2)
     go func() {
         defer conn1.Close()
