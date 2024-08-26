039a6df92bb1f5f6b774355f4f55f97379fd174e
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-26 10:29:56 +0800

    Update conn.go

--- pkg/handle/conn.go
+++ pkg/handle/conn.go
@@ -8,10 +8,12 @@ import (
 func Conn(conn1, conn2 net.Conn) {
     done := make(chan struct{}, 2)
     go func() {
+        defer conn1.Close()
         io.Copy(conn1, conn2)
         done <- struct{}{}
     }()
     go func() {
+        defer conn2.Close()
         io.Copy(conn2, conn1)
         done <- struct{}{}
     }()
